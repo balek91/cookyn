88d0b8e5bd2a5da9ffea5ca3a827d4e495dc4cf5
Balek91 <antoinefaller.a@gmail.com>
2018-10-23 01:57:05 +0200

    gestion des mots de passe dans profil

--- screens/ModifyUserScreen.js
+++ screens/ModifyUserScreen.js
@@ -16,6 +16,7 @@ import { WebBrowser } from 'expo';
 import Axios from 'axios';
 import { MonoText } from '../components/StyledText';
 
+
 export default class ModifyUserScreen extends React.Component {
   static navigationOptions = {
     header: null,
@@ -26,6 +27,7 @@ export default class ModifyUserScreen extends React.Component {
     this.passwordField = this.passwordField.bind(this);
     this.changePassword = this.changePassword.bind(this);
     this.verifNewPassword = this.verifNewPassword.bind(this);
+    this.regexNewPassword = this.regexNewPassword.bind(this);
     this.state = {
       id: '',
       nom : '',
@@ -37,9 +39,14 @@ export default class ModifyUserScreen extends React.Component {
       newPassword : '',
       confirmPassword : '',
       showChangePassword: false,
-      borderColorConfirmPassword : 'green',
+      borderColorConfirmPassword : 'gray',
+      borderColorNewPassword : 'gray',
+      statusNewPassword :false,
+      statusConfirmPassword : false
     }
  }
+
+
 
   render() {
     const { navigation } = this.props;
@@ -56,12 +63,6 @@ export default class ModifyUserScreen extends React.Component {
         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
         onChangeText={(nom) => this.setState({nom})}
         value={this.state.nom}
-        />
-        <Text>Username: </Text>      
-        <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(user) => this.setState({user})}
-        value={this.state.user}
         />
         <Text>Prenom:</Text>
         <TextInput
@@ -114,15 +115,15 @@ export default class ModifyUserScreen extends React.Component {
           />
           <Text>Nouveau Mot de Passe:</Text>
           <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(newPassword) => this.setState({newPassword})}
+          style={this.newPasswordStyle()}
+          onChangeText={(newPassword) => this.regexNewPassword(newPassword)}
           secureTextEntry={true} 
           />
           <Text>Confirmer le Mot de passe:</Text>
           <TextInput
           style={this.verifNewPasswordStyle()}
           secureTextEntry={true} 
-          onChangeText = {this.verifNewPassword}
+          onChangeText =   {(confirmPassword) => this.verifNewPassword(confirmPassword)}
 
           />
         </View>
@@ -136,12 +137,53 @@ verifNewPasswordStyle = function(options) {
   return {
     height: 40, 
     borderColor: this.state.borderColorConfirmPassword,
-     borderWidth: 2
+     borderWidth: 1
   }
 }
 
-verifNewPassword(){
-  (confirmPassword) => this.setState({confirmPassword})
+newPasswordStyle = function(options) {
+  return {
+    height: 40, 
+    borderColor: this.state.borderColorNewPassword,
+     borderWidth: 1
+  }
+}
+
+
+
+verifNewPassword(confirmPassword1){
+  this.setState({
+    confirmPassword : confirmPassword1
+  })
+  if(this.state.newPassword == confirmPassword1){
+    this.setState({
+      borderColorConfirmPassword : 'green',
+      statusConfirmPassword : true
+  });
+  }else{
+    this.setState({
+      borderColorConfirmPassword : 'red',
+      statusConfirmPassword : true
+  });
+  }
+}
+
+regexNewPassword(newPass){
+  this.setState({
+    newPassword : newPass
+  })
+  var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+   if(re.test(newPass)){
+    this.setState({
+      borderColorNewPassword : 'green',
+      statusNewPassword : true
+  });
+   }else{
+    this.setState({
+      borderColorNewPassword : 'red',
+      statusNewPassword : false
+  });
+   }
 }
 
   updateUser(){
@@ -158,6 +200,19 @@ verifNewPassword(){
         passwordUser : this.state.oldPassword,
         newPassword : this.state.newPassword
         }
+        if(this.state.statusNewPassword && this.state.statusConfirmPassword){
+          Axios.post('http://51.75.22.154:8080/General/user/UpdateUser',body).then(response =>{
+            if(response.data.idUser != null ){
+              this.props.navigation.state.params.onNavigateBack()
+              this.props.navigation.navigate('Profil')
+            }else{
+              alert("probleme dans l\'ancien mot de passe")
+            }
+        }
+        ) 
+        }else{
+          alert('probleme dans le nouveau mot de passe')
+        }
     }else{
       body = {
         idUser: this.state.id,
@@ -167,13 +222,18 @@ verifNewPassword(){
         usernameUser : this.state.user,
         villeUser: this.state.ville
         }
+        Axios.post('http://51.75.22.154:8080/General/user/UpdateUser',body).then(response =>{
+          if(response.data.idUser != null ){
+            this.props.navigation.state.params.onNavigateBack()
+            this.props.navigation.navigate('Profil')
+          }else{
+            alert('erreur')
+          }
+
+      }
+      ) 
     }
-console.log(body)
-    Axios.post('http://51.75.22.154:8080/General/user/UpdateUser',body).then(response =>{
-        this.props.navigation.state.params.onNavigateBack()
-        this.props.navigation.navigate('Profil')
-    }
-    ) 
+
   }
 
   componentDidMount() {
