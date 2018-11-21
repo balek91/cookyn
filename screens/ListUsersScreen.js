b1e06a9925fc3e845fbb2cdf6cf515d5f3bf5ab7
Balek91 <antoinefaller.a@gmail.com>
2018-11-22 00:06:19 +0100

    refonte de la page listUser

--- screens/ListUsersScreen.js
+++ screens/ListUsersScreen.js
@@ -1,92 +1,50 @@
-import React from 'react';
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
-import Axios from 'axios';
-import { MonoText } from '../components/StyledText';
-
+import React from 'react'
+import { FlatList} from 'react-native'
+import Touchable from '../components/Touchable'
+import ViewContainer from '../components/ViewContainer'
+import ScrollViewCustom from '../components/ScrollViewContainer'
 
 export default class ListUsersScreen extends React.Component {
-  constructor() {
-    super()
-    this.onPress = this.onPress.bind(this);
-    this.state = {
-      users : [],
-      namePage : 'Test'
+
+    state = {
+        users: []
     }
- }
- static navigationOptions = ({navigation}) => {
-  return {
-    title: `${navigation.state.params.title}`
-    };
-};
 
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: `${navigation.state.params.title}`
+        }
+    }
 
- drawContent(contact, index) {
-  return (
-      <TouchableHighlight key={index} onPress={()=> {this.onPress(contact)}}>
-          <View style={styles.contact}>
-              <View>
-                  <Text style={styles.contactName}>{contact.nomUser} {contact.prenomUser}</Text>
-              </View>
-          </View>
-      </TouchableHighlight>
-  );
-}
+keyExtractor = item => item.idUser.toString()
 
 render() {
-  return (
-      <View style={styles.container}>
-          <ScrollView style={styles.wrapper}>
-              {this.state.users && this.state.users.map((user, index) => {
-                  return this.drawContent(user, index)
-              })}
-          </ScrollView>
-      </View>)
+    const {users} = this.state
+    return (
+        <ViewContainer>
+            <ScrollViewCustom>
+                {users ? (
+                    <FlatList
+                        data={users}
+                        keyExtractor={this.keyExtractor}
+                        renderItem={({ item }) => (
+                            <Touchable text={`${item.prenomUser} ${item.nomUser}`} onPressFunction={() => { this.onPress(item) }} />
+                        )} />) : (null)}
+            </ScrollViewCustom>
+        </ViewContainer>
+        )
 }
-  onPress(contact) {
-      this.props.navigation.navigate("ProfilUser");
-   //   this.props.navigation.state.params.backToProfil(contact)
+    onPress = (contact) => {
+        this.props.navigation.navigate('ProfilUser')
     }
 
-  componentDidMount() {
-    const { navigation } = this.props;
-   // console.log(navigation.getParam('users')[0].idUser);
-    this.setState({
-      users : navigation.getParam('users')
-    })
-    const {setParams} = this.props.navigation;
-    setParams({title: navigation.getParam('namePage')});
+    componentDidMount() {
+        const { navigation } = this.props;
+        console.log(navigation.getParam('users'))
+        this.setState({
+            users: navigation.getParam('users')
+        })
+        const { setParams } = this.props.navigation;
+        setParams({ title: navigation.getParam('namePage') })
+    }
 }
-}
-
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      padding: 10
-  },
-  wrapper: {
-      flex: 1,
-      marginBottom: 10
-  },
-  contact: {
-      flexDirection: 'row',
-      borderWidth: 1,
-      borderRadius: 6,
-      borderColor: 'black',
-      height : 50,
-      marginBottom: 10,
-      justifyContent: 'center',
-      alignItems: 'center'
-  },
-  contactName: {
-      fontWeight: '600',
-      fontSize: 30
-  }
-});
-
