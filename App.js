0c9af37cadabd743c410412823826fa3534ba919
Balek91 <antoinefaller.a@gmail.com>
2018-11-22 15:03:26 +0100

    suppression des component expo de app.js

--- App.js
+++ App.js
@@ -1,87 +1,33 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import { createSwitchNavigator} from 'react-navigation';
-import { createRootNavigator} from './navigation/AppNavigator.js'
-import { isSignedIn} from  './components/Auth.js';
-import MainTabNavigator from './navigation/MainTabNavigator';
-import NotConnected from './navigation/SignedOutNavigator';
-import QuickPicker from 'quick-picker';
+import QuickPicker from 'quick-picker'
+import React from 'react'
+import styled from 'styled-components'
+import { isSignedIn } from './components/Auth.js'
+import { createRootNavigator } from './navigation/AppNavigator.js'
 
+const StyledView = styled.View`
+flex :1;
+background-color: #fff`
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
-    
-  };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      signedIn:false,
+    signedIn: false,
     checkedSignIn: false
-     }
+
   }
 
-  componentDidMount(){
+  componentDidMount() {
     isSignedIn()
-    .then(res => this.setState({signedIn: res, checkedSignIn: true}))
-    .catch(err => console.log("An error occured " + err ));
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(err => console.log('An error occured ' + err));
 
   }
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      const Layout = createRootNavigator(this.state.signedIn);
-      return(
-        <View style={styles.container}><Layout /> <QuickPicker /></View> )
-    }
+
+    const Layout = createRootNavigator(this.state.signedIn);
+    return (
+      <StyledView><Layout /> <QuickPicker /></StyledView>)
+
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
- const navigator = createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  SignedIn: MainTabNavigator,
-  SignedOut: NotConnected
-  
-});
- 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
