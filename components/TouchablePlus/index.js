6c5a74f6b1f96b6012a3458ee8a4f10cf308b367
Steve Togboga <steve@MacBook-Pro-de-Steve.local>
2018-11-26 10:34:15 +0100

    button add etape recette image

---
+++ components/TouchablePlus/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {Image} from 'react-native'
+
+import addblue from '../../assets/icons/addblue.png'
+
+const StyledTouchable = styled.TouchableOpacity
+`margin-vertical: 10;
+padding-vertical: 13;
+`
+
+const StyledImage = styled.Image
+`
+width: 50;
+ height: 50;
+`
+
+export default class TouchablePlus extends Component {
+    static propTypes = {
+        onPressFunction: PropTypes.any,
+    }
+
+    render(){
+        const {onPressFunction } = this.props;
+        return (
+            <StyledTouchable onPress={onPressFunction}>
+                 <StyledImage source={require('../../assets/icons/addblue.png')} />
+            </StyledTouchable>   
+        );
+    }
+}
