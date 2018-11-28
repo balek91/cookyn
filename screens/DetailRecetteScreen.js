f858c5ec090da2aac47490c417733edcbc1777af
Steve Togboga <steve@MacBook-Pro-de-Steve.local>
2018-11-28 17:56:55 +0100

    page détail recette

--- screens/DetailRecetteScreen.js
+++ screens/DetailRecetteScreen.js
@@ -4,11 +4,10 @@ import styled from 'styled-components/native'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 
-import ViewTwoItem from '../components/ViewsAlignItemRow'
 
 const Content = styled.ScrollView`
 flex: 3;
-backgroundColor: #E88110;
+backgroundColor: #F5FCFF;
 width:100%
 `
 
@@ -23,8 +22,26 @@ const Header = styled.View`
 flex: 1;
 justifyContent: center;
 alignItems: center;
-backgroundColor: #F5FCFF;
+backgroundColor: #E88110;
 `
+
+const Footer = styled.View`
+flex: 1;
+justifyContent: center;
+alignItems: center;
+backgroundColor: #E88110;
+`
+
+const StyledViewArray = styled.View`
+justifyContent: center;
+alignItems: flex-start;
+backgroundColor: #F5FCFF;
+border-radius:10;
+border-width:0.5;
+margin: 0px 20px 20px 20px
+`
+//alignSelf:flex-start;
+
 
 const StyledImage = styled.Image
   `
@@ -33,20 +50,20 @@ const StyledImage = styled.Image
   border-radius:75;
 `
 
-const AlignContent = styled.View
+const StyledIcon = styled.Image
+  `
+  height : 50;
+  width : 50;
+  margin:4%;
+`
+
+const AlignContentLeft = styled.View
 `
 flex:1
-justifyContent: center;
+justifyContent: flex-start;
 alignItems: center;
 flexDirection:row;
 flex-grow: 1;
-`
-
-const StyledTitle = styled.View
-`
-justifyContent: center;
-alignItems: center;
-padding:2%
 `
 
 
@@ -56,11 +73,30 @@ fontWeight: bold;
 fontSize: 24;
 `
 
-const StyledTextItalic = styled.Text
+const StyledText = styled.Text
   `
-fontWeight: 100;
-font-style: italic;
+fontWeight: 300;
 fontSize: 24;
+`
+const StyledTextArray = styled.Text
+  `
+fontWeight: 300;
+fontSize: 16;
+margin:15px 0px 10px 25px;
+`
+
+const LabelLeft = styled.Text
+`
+fontWeight: 300;
+fontSize: 16;
+margin:15px 0px 10px 25px;
+`
+
+const LabelRight = styled.Text
+  `
+fontWeight: 300;
+fontSize: 16;
+margin:15px 0px 10px 5px;
 `
 
 
@@ -68,12 +104,17 @@ export default class DetailScreen extends React.Component {
 
 	state = {
 		idRecette: this.props.navigation.getParam('recette').idRecette,
-		data : PropTypes.array
+		data : PropTypes.array,
 	}
 
 	render() {
 		const { data } = this.state
-		data.etapes == undefined ? console.log('error') : console.log(data.recette.libelleRecette)
+		const IconCourse =require('../assets/icons/liste_de_courses.png')
+		const IconFb = require('../assets/icons/reseaux_sociales/facebook.png')
+		const IconMail = require('../assets/icons/reseaux_sociales/mail.png')
+		const IconTwitter = require('../assets/icons/reseaux_sociales/twitter.png')
+		const IconWA = require('../assets/icons/reseaux_sociales/whatsapp.png')
+
 		if (data.etapes == undefined) {
 			return (
 				<StyledView>
@@ -81,33 +122,64 @@ export default class DetailScreen extends React.Component {
 				</StyledView>
 			)
 		} else {
+		const ingredients = data.ingredients
+		const etapes = data.etapes
+		etapes. map((item) => {
+			console.log(item.indexEtape + ' - ' + item.descriptionEtape)
+		})
 		return (
 			<StyledView>
-				 <Header>
-				 <StyledTextBold>{data.recette.libelleRecette}</StyledTextBold>
-				 </Header>
 				 <Content>
-					 <AlignContent>
-					 <Label width={180} height={40} text={'Catégorie Recette :'} />
-					 <Label width={190} height={40} text={data.recette.catRecette} />
-					 </AlignContent>
-					 <AlignContent>
-					 <Label width={180} height={40} text={'Difficulté Recette :'} />
-					 <Label width={190} height={40} text={data.recette.diffRecette} />
-					 </AlignContent>
-					 <AlignContent>
-					 <Label width={180} height={40} text={'Prix Recette :'} />
-					 <Label width={190} height={40} text={data.recette.prix} />
-					 </AlignContent>
-					 <AlignContent>
-					 <Label width={180} height={40} text={'Temps de préparation :'} />
-					 <Label width={190} height={40} text={data.recette.tempPrepaRecette + 'min'} />
-					 </AlignContent>
-					 <StyledTitle>
-						 <StyledTextBold>{'Étapes'}</StyledTextBold>
-					 </StyledTitle>
-					 
+					<Header>
+						<StyledTextBold>{data.recette.libelleRecette}</StyledTextBold>
+					</Header>
+					 <AlignContentLeft>
+					 <LabelLeft>{'Catégorie Recette :'}</LabelLeft>
+					 <LabelRight>{data.recette.catRecette}</LabelRight>
+					 </AlignContentLeft>
+					 <AlignContentLeft>
+					 <LabelLeft>{'Difficulté :'}</LabelLeft>
+					 <LabelRight>{data.recette.diffRecette}</LabelRight>
+					 </AlignContentLeft>
+					 <AlignContentLeft>
+					 <LabelLeft>{'Prix estimé :'}</LabelLeft>
+					 <LabelRight>{data.recette.prix}</LabelRight>
+					 </AlignContentLeft>
+					 <AlignContentLeft>
+					 <LabelLeft>{'Temps de préparation :'}</LabelLeft>
+					 <LabelRight>{data.recette.tempPrepaRecette + 'min'}</LabelRight>
+					 </AlignContentLeft>
+					 <AlignContentLeft>
+						 <StyledIcon source={IconCourse} />
+						 <StyledText>{'INGRÉDIENTS'}</StyledText>
+					 </AlignContentLeft>
+					 <StyledViewArray>
+						 {ingredients.map((item, index) => {
+							return(<StyledTextArray key={'i'+index}>{item.ingredient.libelleIngredient + ' (' + item.quantite + ' ' + item.unite.libelleUnite + ')'}</StyledTextArray>)
+
+						 })}
+					 </StyledViewArray>
+					 <AlignContentLeft>
+						 <StyledIcon source={IconCourse} />
+						 <StyledText>{'ÉTAPES'}</StyledText>
+					 </AlignContentLeft>
+					 <StyledViewArray>
+						 {etapes.map((item, index) =>{
+							 return(<StyledTextArray key={'e'+index}>{item.indexEtape + ' - ' + item.descriptionEtape}</StyledTextArray>)
+						 })}
+					 </StyledViewArray>
+					 <Footer>
+					<StyledTextBold>{'Partagez cette recette'}</StyledTextBold>
+					<AlignContentLeft>
+					<StyledIcon source={IconFb} />
+					<StyledIcon source={IconMail} />
+					<StyledIcon source={IconTwitter} />
+					<StyledIcon source={IconWA} />
+
+					</AlignContentLeft>
+				</Footer>
 				</Content>
+				
 				
 			</StyledView>
 		)}	
